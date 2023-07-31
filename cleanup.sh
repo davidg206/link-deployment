2b4321c855dcc7d5e15e7c12ba5257d0dc539443
David Godfrey <david@palatialxr.com>
2023-07-31 00:31:16 +0000

    Add filetype for sites-enabled deletion

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 sudo certbot delete --cert-name $1.palatialxr.com
-
 sudo systemctl stop dom_$1 server_$1
 sudo systemctl disable dom_$1 server_$1
 sudo rm /etc/systemd/system/dom_${1}.service
 sudo rm /etc/systemd/system/server_${1}.service
 sudo rm /etc/nginx/sites-available/$1.conf
-sudo rm /etc/nginx/sites-enabled/$1
+sudo rm /etc/nginx/sites-enabled/$1.conf
 sudo rm -rf ~/servers/$1

7464e84273c3810979b684b18468a7f073815804
David Godfrey <david@palatialxr.com>
2023-08-06 02:06:42 +0000

    Update module

--- cleanup_impl.sh
+++ cleanup_impl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#sudo certbot delete --cert-name $1.palatialxr.com
 sudo systemctl stop dom_$1
 sudo systemctl stop server_$1
 sudo systemctl disable dom_$1
@@ -9,5 +8,5 @@ sudo rm /etc/systemd/system/dom_${1}.service
 sudo rm /etc/systemd/system/server_${1}.service
 sudo rm /etc/nginx/sites-available/$1.conf
 sudo rm /etc/nginx/sites-enabled/$1.conf
-#sudo rm -rf ~/servers/$1
+sudo rm -rf ~/servers/$1
 sudo nginx -s reload

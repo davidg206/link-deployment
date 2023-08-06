47d7fb2d677b0254adfe95001db5ba3110a812dd
David Godfrey <david@palatialxr.com>
2023-08-06 02:12:01 +0000

    Update

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for arg in "$@"; do
-    cleaned_arg=$(echo "$arg" | sed 's/^server_//; s/^dom_//')
-    # Call the other script and pass the current argument
-    ./cleanup_impl.sh "$cleaned_arg"
-done

5c03c6c984e5d693734008c4638202367fb4ce7a
David Godfrey <david@palatialxr.com>
2023-09-10 20:02:50 +0000

    Delete servers only if they exist

--- util/clear_backend_files.sh
+++ util/clear_backend_files.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
-sudo systemctl stop server_$1
-sudo systemctl disable server_$1
 if [ -f "/etc/systemd/system/server_${1}.service" ]; then
+  sudo systemctl stop server_$1
+  sudo systemctl disable server_$1
   sudo rm /etc/systemd/system/server_${1}.service
 fi
 sudo systemctl daemon-reload
--- util/clear_frontend_files.sh
+++ util/clear_frontend_files.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
-sudo systemctl stop dom_$1
-sudo systemctl disable dom_$1
 if [ -f "/etc/systemd/system/dom_${1}.service" ]; then
+  sudo systemctl stop dom_$1
+  sudo systemctl disable dom_$1
   sudo rm /etc/systemd/system/dom_${1}.service
 fi
 sudo systemctl daemon-reload
